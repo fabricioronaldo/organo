0e12d37dd8fb516aa17def92dcf37986e1c4fed0
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-04-14 21:51:04 -0300

    feat: Criando a lista suspensa do formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,9 @@
 import CampoTexto from '../CompoTexto/CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './formulario.css';
 
 const Formulario = () => {
-    
+    const times = ['Progrmação','Front-End','Data Science','Devops','UX e Design','Mobile','Inovação e Gestão'];
     return (
         <section className='formulario'>
             <form>
@@ -10,6 +11,7 @@ const Formulario = () => {
                 <CampoTexto label="None" placeholder="Digite seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem"placeholder="Informe o endereço de imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
