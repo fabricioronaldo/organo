bd64cb03d9046cac5d3ee04a1c563c73a959f747
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-04-24 23:15:55 -0300

    feat: Inicio da validação do campo nome

--- src/componentes/CompoTexto/CampoTexto.js
+++ src/componentes/CompoTexto/CampoTexto.js
@@ -2,10 +2,15 @@ import './CampoTexto.css';
 
 const CampoTexto = (props)=> {
 
+    let valor = ''
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
     return (
         <div class="form__campo">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>    
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>    
         </div>
     )
 }
